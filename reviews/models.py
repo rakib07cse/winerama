56bba4a4937c4f3b9e8234e15691c1b6331c6d29
Rakibul hassan <rakib07cse@gmail.com>
2017-07-26 12:12:51 +0600

    Add init project

---
+++ reviews/models.py
@@ -0,0 +1,32 @@
+from __future__ import unicode_literals
+
+from netaddr.strategy.eui48 import mac_bare
+
+import numpy as np
+from django.db import models
+
+# Create your models here.
+
+class Wine(models.Model):
+    name = models.CharField(max_length=200)
+
+    def average_rating(self):
+        all_rating = map(lambda x:x.rating,self.review_set.all())
+        return np.mean(all_rating)
+
+    def __unicode__(self):
+        return self.name
+
+class Review(models.Model):
+    RATING_CHOICES=(
+        (1,'1'),
+        (2,'2'),
+        (3,'3'),
+        (4,'4'),
+        (5,'5'),
+    )
+    wine = models.ForeignKey(Wine)
+    pub_date = models.DateTimeField('date published')
+    user_name = models.CharField(max_length=100)
+    comment = models.CharField(max_length=200)
+    rating = models.IntegerField(choices=RATING_CHOICES)
